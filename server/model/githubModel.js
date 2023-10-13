79415847378d327984428f473c156753182771d5
Prabhat kumar gupta <guptajim3636@gmail.com>
2023-10-13 23:34:49 +0530

    completed

--- server/model/githubModel.js
+++ server/model/githubModel.js
@@ -1,24 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const githubSchema = mongoose.Schema({
-    id: Number,
-    name: String,
-    html_url: String,
-    description: String,
-    created_at: String,
-    open_issues: Number,
-    watchers: Number,
-    owner: {
+const githubSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  html_url: String,
+  description: String,
+  created_at: String,
+  open_issues: Number,
+  watchers: Number,
+  owner: {
+    type: {
       id: Number,
       avatar_url: String,
       html_url: String,
       type: String,
       site_admin: Boolean
-    }
+    },
+    default: {}
+  }
 });
 
-const GitHubModel=mongoose.model("github_data",githubSchema)
+const GitHubModel = mongoose.model('GitHubModel', githubSchema);
 
-module.exports={
-    GitHubModel
-}
+module.exports = { GitHubModel };
--- server/routes/githubRoutes.js
+++ server/routes/githubRoutes.js
@@ -1,72 +1,75 @@
-const express=require("express");
-const axios = require('axios');
-const { GitHubModel }=require("../model/githubModel")
-const githubRouter=express.Router();
+const express = require("express");
+const axios = require("axios");
+const { GitHubModel } = require("../model/githubModel");
+const githubRouter = express.Router();
 
-const githubController=require("../controller/githubController")
+const githubController = require("../controller/githubController");
 
-githubRouter.post("/github",async(req,res)=>{
-    try{
-    
-       
-       // console.log(999,req.body);
-     const response = await axios.get(req.body.url);
-    
-     const vvv=response.data;
+githubRouter.post("/github", async (req, res) => {
+  try {
+ 
+    const response = await axios.get(req.body.url);
 
-     
-        const obj = vvv.map(e => {
-         return e
-        });
+    const repositories = response.data; 
 
-        let val=obj.forEach(e => {
-           
-            console.log(11,e)
-            const info={id:e.id,name:e.name}
-            return info
-        });
-        res.status(200).json(val)
+    for (const repo of repositories) {
+      const {
+          id,
+          name,
+          html_url,
+          description,
+          created_at,
+          open_issues,
+          watchers,
+          owner
+      } = repo;
 
-        
-    //  const {id,
-    //     name,
-    //     html_url,
-    //     description,
-    //     created_at,
-    //     open_issues,
-    //     watchers} = obj.forEach(element => element);
+      const { id: ownerId, avatar_url, html_url: ownerHtmlUrl, type, site_admin } = owner;
 
-     
+      // Create an instance of the GitHubModel
+      const data = new GitHubModel({
+          id,
+          name,
+          html_url,
+          description,
+          created_at,
+          open_issues,
+          watchers,
+          owner: {
+              id: ownerId,
+              avatar_url,
+              html_url: ownerHtmlUrl,
+              type,
+              site_admin
+          }
+      });
 
-        // console.log(888,id,
-        //     name,
-        //     html_url,
-        //     description,
-        //     created_at,
-        //     open_issues,
-        //     watchers)
+   
+      // Save the data to MongoDB
+      await data.save();
+  }
 
-    //  const data =new GitHubModel({id,
-    //     name,
-    //     html_url,
-    //     description,
-    //     created_at,
-    //     open_issues,
-    //     watchers
-    // })
-     //await data.save()
-     //console.log(data)
+  res.send("GitHub data saved successfully.");
 
-     
-     
-  
+  } catch (err) {
+    console.log("working")
+    res.send(err.message);
+  }
+});
 
-    }catch(err)
-    {
-        res.send(err.message)
-    }
+//get router
+githubRouter.get("/github/:id",async(req,res)=>{
+  try{
+    let id=req.params.id
+    const data = await GitHubModel.findOne({ id});
+    res.status(200).json(data)
+
+  }catch(err)
+  {
+    res.status(401).json(err.message)
+  }
 })
 
-module.exports={
-    githubRouter
-}
+module.exports = {
+  githubRouter,
+};
